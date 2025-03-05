ffeb0c6431beca9647651d36f0446e96eb16c34c
NFajardo98 <65809295+NFajardo98@users.noreply.github.com>
2025-03-05 15:00:07 +0100

    CollectionsForm component created (Error to fix)

---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { Separator } from '../ui/separator'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+// Creamos el schema
+const formSchema = z.object({
+    title: z.string().min(2).max(20),
+    description: z.string().min(2).max(500).trim(),
+    image: z.string(),
+  });
+
+  
+const CollectionForm = () => {
+      // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image:"",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  const onSubmit = async(values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  return (
+    <div className="p-10">
+      <p className="text-heading2-bold">Create Collection</p>
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+      return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          title="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+    </div>
+  )
+}
+
+export default CollectionForm
